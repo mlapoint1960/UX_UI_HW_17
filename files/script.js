a12864419a00165dc531f673a3654f1f7f76d128
Mark LaPoint <marklapoint@gmail.com>
2022-07-07 11:47:01 -0600

    site updates
    lottie added to header & footer fixes

---
+++ files/script.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets6.lottiefiles.com/private_files/lf30_q7dfnats.json' //lottie file path
+
+})
